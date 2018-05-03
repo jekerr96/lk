16a5d3274ed3f1af9674407a316a7beecdc21c85
jekerr96 <jekerr96@gmail.com>
2018-05-03 13:39:16 +0700

    Попытка создать чат, но РКН НЕ ДАЛИ!!!

---
+++ managers/include/chat.php
@@ -0,0 +1,15 @@
+<div class="open_chat">
+
+</div>
+<div class="block_chat">
+<div class="head_chat">
+</div>
+<div class="body_chat">
+
+</div>
+<div class="tools_chat">
+  <div class="block_msg" contenteditable="true">
+
+  </div>
+</div>
+</div>
---
+++ managers/include/getMyName.php
@@ -0,0 +1,3 @@
+<?
+  echo $_SESSION["fio"];
+?>
